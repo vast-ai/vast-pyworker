9bd920549e9f4d873e762794f4f5e04bfc057056
Nick Greenspan <nicholasg101@gmail.com>
2023-10-05 14:20:29 -0500

    added crypto authentication, haven't been able to test yet

--- start_server.sh
+++ start_server.sh
@@ -9,7 +9,7 @@ then
     pip install pycryptodome
     mkdir /home/workspace
     cd /home/workspace
-    git clone -b authentication https://github.com/vast-ai/vast-pyworker;
+    git clone https://github.com/vast-ai/vast-pyworker;
     touch ~/.no_auto_tmux
     touch /root/hasbooted
 fi
