d3c707fab6fa36c790dcbf241b8711257bd9ab61
Nicholas Greenspan <nicholasg101@gmail.com>
2023-10-12 19:59:08 -0400

    back to cloning the main branch

--- start_server.sh
+++ start_server.sh
@@ -10,7 +10,7 @@ then
     pip install pycryptodome
     mkdir /home/workspace
     cd /home/workspace
-    git clone -b handle-error-test https://github.com/vast-ai/vast-pyworker
+    git clone https://github.com/vast-ai/vast-pyworker
     touch ~/.no_auto_tmux
     touch /root/hasbooted
 fi
