9fae23f16e5064a3bb102a4a324b4497dd090dc3
Nicholas Greenspan <nicholasg101@gmail.com>
2023-10-17 11:24:04 -0500

    now testing with real REPORT_ADDR

--- start_server.sh
+++ start_server.sh
@@ -18,7 +18,7 @@ cd /home/workspace/vast-pyworker
 
 export SERVER_DIR="/home/workspace/vast-pyworker"
 export PATH="/opt/conda/bin:$PATH"
-export REPORT_ADDR="https://shirt-hidden-professional-civil.trycloudflare.com"
+export REPORT_ADDR="https://run.vast.ai"
 
 if [ -z "$REPORT_ADDR" ] || [ -z "$MODEL_CMD" ] || [ -z "$AUTH_PORT" ]; then
   echo "REPORT_ADDR, MODEL_CMD, AUTH_PORT env variables must be set!"
