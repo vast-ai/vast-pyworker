7b4910ba5b513c7e31b8728fec78ee09a02445d5
Nick Greenspan <nicholasg101@gmail.com>
2023-10-25 07:34:11 -0500

    modifed start up

--- start_server_ooba.sh
+++ start_server_ooba.sh
@@ -4,12 +4,13 @@ date;
 env | grep _ >> /etc/environment;
 
 if [ ! -f /root/hasbooted ]
-then    
+then
     pip install flask
     pip install nltk
     pip install pycryptodome
     pip install accelerate -U
     pip install protobuf
+    python -m pip install git+https://github.com/jllllll/exllama
     mkdir /home/workspace
     cd /home/workspace
     git clone -b ooba-compat https://github.com/vast-ai/vast-pyworker
@@ -20,7 +21,7 @@ cd /home/workspace/vast-pyworker
 
 export SERVER_DIR="/home/workspace/vast-pyworker"
 export PATH="/opt/conda/bin:$PATH"
-export REPORT_ADDR="https://run.vast.ai"
+export REPORT_ADDR="https://falling-vaccine-spokesman-joining.trycloudflare.com"
 export MASTER_TOKEN="mtoken"
 export CONTAINER_ID=0
 export AUTH_PORT=3000
