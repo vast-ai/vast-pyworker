83689911a6949aa7c1049c14c90ecaba80e6ba0e
Nicholas Greenspan <nicholasg101@gmail.com>
2023-11-07 15:54:09 -0600

    now added loading message to all backends

--- ooba/launch.sh
+++ ooba/launch.sh
@@ -20,7 +20,8 @@ echo "$MODEL_PATH"
 cd /src
 if [ ! -d "$MODEL_PATH" ]
 then
+echo "starting model download" > $SERVER_DIR/infer.log
     python3 /app/download-model.py $MODEL_USER/$MODEL_NAME > $SERVER_DIR/download.log 2>&1
 fi
-python3 /app/server.py --extensions api --api-blocking-port 5001 --api-streaming-port 5002 --model $MODEL_PATH --listen > $SERVER_DIR/infer.log 2>&1 &
+python3 /app/server.py --extensions api --api-blocking-port 5001 --api-streaming-port 5002 --model $MODEL_PATH --listen >> $SERVER_DIR/infer.log 2>>&1 &
 echo "done"
--- sdauto/launch.sh
+++ sdauto/launch.sh
@@ -10,6 +10,7 @@ then
         sudo apt install git-lfs
         git lfs install
         cd /home/workspace
+        echo "starting model download" > $SERVER_DIR/infer.log
         git clone https://$HF_USERNAME:$HUGGING_FACE_HUB_TOKEN@huggingface.co/$HF_MODEL_USER/$HF_MODEL_REPO
     fi
     touch /home/workspace/init_launch
@@ -23,5 +24,5 @@ then
 else
     CKPT_ARG=""
 fi
-python /stable-diffusion-webui/launch.py $CKPT_ARG $MODEL_ARGS --api-log --nowebui --port 5000 > $SERVER_DIR/infer.log 2>&1 &
+python /stable-diffusion-webui/launch.py $CKPT_ARG $MODEL_ARGS --api-log --nowebui --port 5000 >> $SERVER_DIR/infer.log 2>>&1 &
 echo "launched model"
