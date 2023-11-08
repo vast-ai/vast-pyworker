e7a73db0c8ccbaa0cd02a192cabcb53090101444
Nicholas Greenspan <nicholasg101@gmail.com>
2023-11-07 18:42:31 -0600

    added to start script

--- start_tgi_test_2.sh
+++ start_tgi_test_2.sh
@@ -1,3 +1,3 @@
 export BACKEND=tgi 
-export MODEL_ARGS="--model-id TheBloke/Llama-2-70B-Chat-GPTQ --quantize gptq" 
+export MODEL_ARGS="--model-id TheBloke/Llama-2-7B-Chat-GPTQ --quantize gptq" 
 wget -O - https://raw.githubusercontent.com/vast-ai/vast-pyworker/main/start_server.sh | bash
