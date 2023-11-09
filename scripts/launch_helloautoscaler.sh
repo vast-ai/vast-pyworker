64fa09960268e38cd1d2aa89d3c6dc7045b73e42
Nicholas Greenspan <nicholasg101@gmail.com>
2023-11-09 15:43:44 -0600

    now using the right branch

--- scripts/launch_helloautoscaler.sh
+++ scripts/launch_helloautoscaler.sh
@@ -1,7 +1,7 @@
 start_server() {
     if [ ! -d "$1" ]
     then
-        wget -O - https://raw.githubusercontent.com/vast-ai/vast-pyworker/main/start_server.sh | bash -s $2
+        wget -O - https://raw.githubusercontent.com/vast-ai/vast-pyworker/helloautoscaler/start_server.sh | bash -s $2
     else
         $1/start_server.sh $2
     fi
