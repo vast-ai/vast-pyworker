9905eed322729b8dd118b95e8e2f6e7e634e3ecb
Nicholas Greenspan <nicholasg101@gmail.com>
2023-11-09 18:25:13 -0600

    now using new scheme for notify

--- notify.py
+++ notify.py
@@ -2,15 +2,16 @@ from utils import send_data
 
 notify_endpoint = "/worker_status/"
 
-def loaded(id, autoscaler_address, load_time, max_perf):
-    data = {"id" : id, "loadtime" : load_time, "max_perf" : max_perf}
+def loaded(data, autoscaler_address, load_time, max_perf):
+    data["loadtime"] = load_time
+    data["max_perf"] = max_perf
     send_data(data=data, url=autoscaler_address, path=notify_endpoint, sender="notify")
 
-def update(id, autoscaler_address, cur_load, num_requests_recieved):
-    data = {"id" : id, "cur_load" : cur_load, "num_requests_recieved" : num_requests_recieved}
+def update(data, autoscaler_address, cur_load, num_requests_recieved):
+    data["cur_load"] = cur_load
+    data["num_requests_recieved"] = num_requests_recieved
     send_data(data=data, url=autoscaler_address, path=notify_endpoint, sender="notify")
 
-def error(id, autoscaler_address, error_msg):
-    data = {"id" : id, "error_msg" : error_msg}
+def error(data, autoscaler_address, error_msg):
+    data["error_msg"] = error_msg
     send_data(data=data, url=autoscaler_address, path=notify_endpoint, sender="notify")
-    pass
