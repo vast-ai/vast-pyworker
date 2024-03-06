6a50a35d7d9f68c924320ca9f137d7560e57df62
Nicholas Greenspan <nicholasg101@gmail.com>
2024-03-05 18:28:46 -0600

    back to main branch

--- scripts/launch_sdauto.sh
+++ scripts/launch_sdauto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "launch_sdauto.sh" | tee -a /root/debug.log
+echo "launch_sdauto2.sh" | tee -a /root/debug.log
 
 SERVER_DIR="/home/workspace/vast-pyworker"
 
@@ -15,29 +15,5 @@ start_server() {
 start_server "$SERVER_DIR" "sdauto"
 deactivate #deactivates the vast-pyworker venv, in preperation for activating the backend specific venv
 
-if [ ! -f "/home/workspace/init_launch" ]
-then
-    rsync --remove-source-files -rlptDu --ignore-existing /venv/ /workspace/venv/
-    if [ -n "$HF_MODEL_REPO" ]
-    then
-        curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-        sudo apt install git-lfs
-        git lfs install
-        cd /home/workspace
-        echo "starting model download" > $SERVER_DIR/infer.log
-        git clone https://$HF_USERNAME:$HUGGING_FACE_HUB_TOKEN@huggingface.co/$HF_MODEL_USER/$HF_MODEL_REPO
-    fi
-    touch /home/workspace/init_launch
-fi
-
-source /workspace/venv/bin/activate
-
-if [ -n "$HF_MODEL_REPO" ]
-then
-    CKPT_ARG="--ckpt /home/workspace/$HF_MODEL_REPO/$HF_MODEL_FILE"
-else
-    CKPT_ARG=""
-fi
-python /stable-diffusion-webui/launch.py $CKPT_ARG $MODEL_ARGS --api-log --nowebui --port 5000 >> $SERVER_DIR/infer.log 2>&1 &
+/opt/ai-dock/bin/init.sh >> $SERVER_DIR/infer.log 2>&1 &
 echo "launched model"
-
